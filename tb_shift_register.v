f9453c4ef1fc0ed56d73d77bd38f9eab7fc74671
saiviswanath038-png <saiviswanath038@gmail.com>
2026-03-19 09:29:04 +0530

    Create tb_shift_register.v

---
+++ tb_shift_register.v
@@ -0,0 +1,60 @@
+`timescale 1ns/1ps
+
+module tb_shift_register;
+
+reg clk;
+reg reset;
+reg [1:0] mode;
+reg serial_in;
+reg [3:0] parallel_in;
+wire [3:0] data_out;
+wire serial_out;
+
+// Instantiate the design
+shift_register uut (
+    .clk(clk),
+    .reset(reset),
+    .mode(mode),
+    .serial_in(serial_in),
+    .parallel_in(parallel_in),
+    .data_out(data_out),
+    .serial_out(serial_out)
+);
+
+// Clock generation
+always #5 clk = ~clk;
+
+initial begin
+    // Initial values
+    clk = 0;
+    reset = 1;
+    mode = 2'b00;
+    serial_in = 0;
+    parallel_in = 4'b0000;
+
+    // Reset
+    #10 reset = 0;
+
+    // Parallel Load
+    #10 mode = 2'b11;
+    parallel_in = 4'b1010;
+
+    // Shift Left
+    #10 mode = 2'b01;
+    serial_in = 1;
+
+    #20;
+
+    // Shift Right
+    #10 mode = 2'b10;
+    serial_in = 0;
+
+    #20;
+
+    // Hold
+    #10 mode = 2'b00;
+
+    #20 $stop;
+end
+
+endmodule
